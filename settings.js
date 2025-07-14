3c9466981b54570f77d7b2ea7a6f48211adbbdf2
BrbBot69 <148257126+BrbBot69@users.noreply.github.com>
2025-07-14 16:19:47 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: '☚⍢⃝☚',
   author: '‎',
-  botName: "Knight Bot",
+  botName: "𝕬𝖛𝖊.𝕭☚⍢⃝☚",
   botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '27767494368', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
